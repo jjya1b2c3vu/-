2b13c50ccbbb8c31e9dfd89fca57a961537257e2
zhonya-j <jjya1b2c3bu@gmail.com>
2021-04-15 00:05:50 +0900

    방문한 리스트에 false가 출력됨

--- boj/18352_특정거리의도시찾기.py
+++ boj/18352_특정거리의도시찾기.py
@@ -1,5 +1,37 @@
 from sys import stdin
+from pprint import pprint
 stdin = open("boj/input.txt","r")
 input = stdin.readline
 N,M,K,X = map(int,input().split())
-print(N,M,K,X)
+
+graph = [ [] for _ in range(N+1) ]
+
+for _ in range(1,M+1):
+    i,j = map(int,input().split())
+    graph[i].append(j) 
+
+visited = [-1]*(N+1)
+
+from collections import deque
+def bfs(v):
+    qu = deque()
+    qu.append(v)
+    visited[v]=0
+    cnt=0
+    while(qu):
+        now_node = qu.popleft()
+        for next_node in graph[now_node]:
+            if visited[next_node]==-1:
+                qu.append(next_node)
+                visited[next_node]=visited[now_node]+1
+                
+bfs(X)
+ans=[]
+for i,v in enumerate(visited):
+    if v==K:
+        ans.append(i)
+if len(ans)==0:
+    print(-1)
+else:
+    for a in sorted(ans):
+        print(a)
